f4f9df0cdd719e6974c61d2054f89990ec892b01
bitonkfir <kfirbiton2@ymail.com>
2021-03-12 18:35:03 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const router = express.Router();
+const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts')
+
 const app_port = process.env.PORT || 3000
 const app = express()
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+
+app.set('view engine','ejs');
+app.use(expressEjsLayout);
+app.use(express.urlencoded({extended : false}));
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
+
+
 app.listen(app_port)
 console.log(`app is running. port: ${app_port}`)
 console.log(`http://127.0.0.1:${app_port}/`)
